86f8bcbb3bcc161ab3b3d50b849cad3e3e3cc2a4
Alex Bispo <alexbispo.artes@gmail.com>
2016-06-06 08:09:35 -0300

    Refactor to Bob's solution.

--- bob/src/bob.clj
+++ bob/src/bob.clj
@@ -4,21 +4,19 @@
   [word]
   (not (re-find #"[A-Za-z]" word)))
 
-(defn is-yell?
+(defn is-a-shout?
    [word]
    (if (not (is-only-number? word))
      (= word (.toUpperCase word))))
 
 (defn is-a-question?
    [word]
-   (and (not (is-yell? word)) (.endsWith word "?")))
+   (and (not (is-a-shout? word)) (.endsWith word "?")))
 
  (defn response-for
    [word]
-   (if (.isEmpty (.trim word))
-     "Fine. Be that way!"
-     (if (is-a-question? word)
-       "Sure."
-       (if (is-yell? word)
-         "Whoa, chill out!"
-         "Whatever."))))
+   (cond
+     (.isEmpty (.trim word)) "Fine. Be that way!"
+     (is-a-question? word) "Sure."
+     (is-a-shout? word) "Whoa, chill out!"
+     :else "Whatever."))
